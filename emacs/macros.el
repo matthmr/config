85fc537c3ab24ecc13f7a90b2363947ce6f0d1a0
mh <observermatth@gmail.com>
2024-10-28 19:02:44 -0300

    up(emacs/mh-bc.el): add `mh/bc-in-modeline` customizable variable for displaying
      `breadcrumb-mode` in the modeline instead
    
    up(emacs/macros.el): move register string values into abbrevs (not in this
      repository)

--- emacs/macros.el
+++ emacs/macros.el
@@ -1,32 +1,3 @@
-;;;; Registers
-
-(set-register ?s '("#+begin_src"
-                   "#+end_src"))
-
-(set-register ?q '("#+begin_quote"
-                   "#+end_quote"))
-
-(set-register ?o '(
-                   "#+TITLE: "
-                   "#+DATE: "
-                   "#+AUTHOR: "))
-
-(set-register ?l '(
-                   "#ifndef LOCK_"
-                   "#  define LOCK_"
-                   ""
-                   ""
-                   ""
-                   "#endif"))
-
-(set-register ?t '(
-                   "struct {"
-                   "};"
-                   ""
-                   "typedef struct ;"))
-
-;;;; Kmacros
-
 (defalias 'mh/copy-thing-at-point
    (kmacro "M-s M-. RET C-@ C-r C-r RET M-w"))
 
--- emacs/mh-bc.el
+++ emacs/mh-bc.el
@@ -1,6 +1,19 @@
 (let ((read-symbol-shorthands '(("bc-" . "breadcrumb-"))))
   (require 'breadcrumb))
 
+(defcustom mh/bc-in-modeline t
+  "Flag for whether to put `bc' in the modeline, instead of the headerline")
+
+;; TODO: not general
+(define-minor-mode mh/bc-mode
+  "Wrapper for bc-local-mode"
+  :init-value nil
+  (if mh/bc-in-modeline
+      (if mh/bc-mode
+          (setq global-mode-string '("%e" (:eval (breadcrumb--header-line))))
+        (setq global-mode-string '("")))
+    (breadcrumb-local-mode)))
+
 (setq breadcrumb-imenu-max-length 0.6)
 
 ;; Little hack to reset bc's cache after starting something that meddles with
@@ -11,5 +24,5 @@
 
 (global-set-key (kbd "C-x C-M-m C-M-i") #'mh/bc-reset)
 
-(add-hook 'prog-mode-hook #'breadcrumb-local-mode)
-(call-interactively #'breadcrumb-local-mode)
+(add-hook 'prog-mode-hook #'mh/bc-mode)
+(call-interactively #'mh/bc-mode)
